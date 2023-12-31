d3b271eef5691103a1c9bf02b5ab5f5257d0466b
rrahmatn <rizkyrahmat292@gmail.com>
2023-12-31 20:31:21 +0700

    dsasd

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const Product = require("./product"); // Pastikan file "product.js" mengandung definisi skema produk
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("Connected to MongoDB");
+
+    app.get("/", (req, res) => {
+        res.send("Express on Vercel");
+      });
+
+    app.post("/product", async (req, res) => {
+      console.log("Result", req.body);
+
+      let data = Product(req.body);
+
+      try {
+        let dataToStore = await data.save();
+        res.status(200).json(dataToStore);
+      } catch (error) {
+        res.status(400).json({
+          status: error.message,
+        });
+      }
+    });
+
+  
+
+    app.get('/api', (req, res) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+      res.end(`Hello! Go to item: <p>hello</p>`);
+    });
+
+    app.get("/product", async (req, res) => {
+      try {
+        let data = await Product.find();
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).json(error.message);
+      }
+    });
+
+    app.patch("/product/:id", async (req, res) => {
+      let id = req.params.id;
+      let updatedData = req.body;
+      let options = { new: true };
+
+      try {
+        const data = await Product.findByIdAndUpdate(id, updatedData, options);
+        res.send(data);
+      } catch (error) {
+        res.send(error.message);
+      }
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      let id = req.params.id;
+      try {
+        const data = await Product.findByIdAndDelete(id);
+        res.json({
+          status: `Deleted the product ${data.pname} from database`,
+        });
+      } catch (error) {
+        res.json(error.message);
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+app.listen(3000, () => {
+  console.log("Connected to server at 3000");
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
-
-// Export the Express API
 module.exports = app;
