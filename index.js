2a2737ff50580a3fd4012e2a9e07213d15c56799
rrahmatn <rizkyrahmat292@gmail.com>
2023-12-31 19:49:31 +0700

    dsadas

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const Product = require("./product"); // Pastikan file "product.js" mengandung definisi skema produk
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors())
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb+srv://david:apaantuh11@pam.spi9mzl.mongodb.net/flutter", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("Connected to MongoDB");
+  })
+  
+  app.get("/product", async (req, res) => {
+    try {
+      let data = await Product.find();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  });
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
